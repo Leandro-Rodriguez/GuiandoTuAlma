416e6af2d227eafcf9ac5d743a7eb3a248b5c328
Leandro Rodriguez <leandroricardorodriguez@gmail.com>
2024-08-24 03:40:31 -0300

    agregados mobile

--- scripts.js
+++ scripts.js
@@ -1,20 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.getElementById('main-header');
+    const menuHamburguesa = document.querySelector('.menu-hamburguesa');
+    const navLinks = document.querySelector('nav ul');
     let lastScrollY = window.scrollY;
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY > lastScrollY) {
-            header.style.top = '-100px';
-        } else {
-            header.style.top = '0';
+        // Verifica si el menú de hamburguesa no está abierto
+        if (!navLinks.classList.contains('open')) {
+            if (window.scrollY > lastScrollY) {
+                header.style.top = '-100px'; // Desaparece al hacer scroll hacia abajo
+            } else {
+                header.style.top = '0'; // Reaparece al hacer scroll hacia arriba
+            }
         }
         lastScrollY = window.scrollY;
     });
 
-    const menuHamburguesa = document.querySelector('.menu-hamburguesa');
-    const navLinks = document.querySelector('nav ul');
-
     menuHamburguesa.addEventListener('click', () => {
         navLinks.classList.toggle('open');
+        // Si el menú se abre, asegura que la barra de navegación esté visible
+        if (navLinks.classList.contains('open')) {
+            header.style.top = '0';
+        }
     });
 });
