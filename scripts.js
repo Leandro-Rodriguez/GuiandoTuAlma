d41c73d8d4d9c3526e04d6ab5aff4f91da1cb092
Leandro Rodriguez <leandroricardorodriguez@gmail.com>
2024-08-27 05:56:18 -0300

    mejoras

--- scripts.js
+++ scripts.js
@@ -24,3 +24,22 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+// Configuración del Intersection Observer
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible'); // Añade la clase 'visible' cuando el elemento está en vista
+      } else {
+        entry.target.classList.remove('visible'); // Opcional: remueve la clase cuando el elemento sale de vista
+      }
+    });
+  });
+  
+  // Selecciona todos los elementos que deseas observar
+  let elementos = document.querySelectorAll('.elementoanim');
+  
+  // Observa cada elemento
+  elementos.forEach(elementoanim => {
+    observer.observe(elementoanim);
+  });
